5e28e15d57f36714e63c6661fff129964fc2b337
junglebooboo <aigarst@stud.ntnu.no>
2019-04-23 17:10:05 +0200

    Changed button name

--- src/components/myButton-happo.jsx
+++ src/components/myButton-happo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from './Button';
+import Button from './myButton';
 
 export const primary = () => <Button type="primary" className="primary" label="Primary">Primary</Button>;
 export const secondary = () => <Button type="secondary" label="Secondary">Secondary</Button>;
