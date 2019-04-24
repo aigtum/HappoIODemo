fd4df20f195a9bae5bf4ea4b08efc5ebe19962ce
junglebooboo <aigarst@stud.ntnu.no>
2019-04-24 20:25:03 +0200

    added file loader

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    module: {
+        rules: [{
+            test: /\.(png|jpe?g|gif)$/,
+            use: [{
+                loader: 'file-loader',
+                options: {},
+            }, ],
+        }, ],
+    },
+};
