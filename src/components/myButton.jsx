705a191b16e89cafa5ece4a12a78060eae32ce15
junglebooboo <aigarst@stud.ntnu.no>
2019-04-24 16:27:59 +0200

    hovering in react is hard

--- src/components/myButton.jsx
+++ src/components/myButton.jsx
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
-import '../App.css';
-
 
 var alertTest = () => {
   alert("test!");
 }
 
 class Button extends Component {
+  
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+        hovering : false
+    };
+}
   render() {
-      return (
-        <button
-          className="primary"
-          onClick={alertTest}>{this.props.label}</button>
-      );
-    }
+    const style = {
+      margin: '2px',
+      borderRadius: '4px',
+      backgroundColor: this.state.hovering ? '#346e5d' : '#55A88F',
+      color: '#FFFFFF',
+      padding: '10px',
+      fontSize: '16px',
+      border: '2px solid #282c34',
+    };
+    
+    const handleMouseEnter = () => this.setState({hovering : true});
+    const handleMouseLeave = () => this.setState({hovering : false});
+    return (
+      <button
+        style={style}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onClick={alertTest}>{this.props.label}</button>
+    );
+  }
 }
 
 export default Button;
