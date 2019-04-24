8f0cff975f91c4e54956d34415234620c4e638a3
junglebooboo <aigarst@stud.ntnu.no>
2019-04-24 15:43:22 +0200

    happo test4

--- src/components/myButton.jsx
+++ src/components/myButton.jsx
@@ -1,39 +1,16 @@
 import React, { Component } from 'react';
+import '../App.css';
 
-var buttonStyle = {
-  'default': {
-    margin: '2px',
-    borderRadius: '4px',
-    backgroundColor: '#55A88F',
-    color: '#FFFFFF',
-    padding: '10px',
-    fontSize: '16px',
-    border: '2px solid #282c34',
-  },
-  'hovered': {
-    margin: '2px',
-    borderRadius: '4px',
-    backgroundColor: '#346e5d',
-    color: '#FFFFFF',
-    padding: '10px',
-    fontSize: '16px',
-    border: '2px solid #282c34',
-  }
-};
-
-var buttonHovered = false;
 
 var alertTest = () => {
   alert("test!");
 }
 
 class Button extends Component {
-    render() {
+  render() {
       return (
         <button
-          onMouseEnter={() => {buttonHovered = true}} 
-          onMouseLeave={() => buttonHovered = false}
-          style={buttonHovered ? buttonStyle.hovered : buttonStyle.default}
+          className="primary"
           onClick={alertTest}>{this.props.label}</button>
       );
     }
