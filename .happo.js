65500614397d7e90275c84176dea744575fddbd3
junglebooboo <aigarst@stud.ntnu.no>
2019-04-25 17:28:43 +0200

    ....

--- .happo.js
+++ .happo.js
@@ -1,8 +1,13 @@
 const { RemoteBrowserTarget } = require('happo.io');
+const path = require('path');
 
 module.exports = {
   apiKey: process.env.REACT_APP_KEY,
   apiSecret: process.env.REACT_APP_SECRET,
+
+  publicFolders: [
+    path.resolve(__dirname, 'src/public'),
+  ],
 
   targets: {
     chrome: new RemoteBrowserTarget('chrome', {
