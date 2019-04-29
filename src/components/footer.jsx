7701b6bbf859b1ec41b3d5344cbd32fc97e41259
junglebooboo <aigarst@stud.ntnu.no>
2019-04-29 16:02:06 +0200

    changed header and footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Footer = (props) => {
     return (
         <footer className="footer">
-            Website info.
+            <p>Demo of the UI Testing Framework, Happo.io</p>
+            <p>Adam Hasselberg and Aigars Tumanis</p>
         </footer>
     )
 };
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,17 +10,19 @@ const styles = {
     alignItems: 'center',
     justifyContent: 'center',
     fontSize: 'calc(5px + 2vmin)',
-    color: 'white'
+    color: 'white',
   },
   'img': {
-    paddingRight: '20px'
+    paddingRight: '25px',
+    width: 'calc(24px + 2vmin)',
   }
 }
 
 const Header = (props) => {
   return (
     <header style={styles.header}>
-      <h1><img src="https://camo.githubusercontent.com/192f268f4e37ae06a0648e3e99a4923be7f45124/68747470733a2f2f686170706f2e696f2f7374617469632f686170706f2d686970706f2e706e67"/>Happo.io Demo</h1>
+      <h1><img style={styles.img} src="https://camo.githubusercontent.com/192f268f4e37ae06a0648e3e99a4923be7f45124/68747470733a2f2f686170706f2e696f2f7374617469632f686170706f2d686970706f2e706e67" alt="happo-logo"/>
+      Happo.io Demo</h1>
     </header>
   )
 };
