13d8c679cd684b22929f22f5391d6264aa97e95f
junglebooboo <aigarst@stud.ntnu.no>
2019-05-01 11:07:03 +0200

    UI changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-
 import './App.css';
 import Header from './components/header.jsx';
 import Footer from './components/footer.jsx';
 import SimpleExpansionPanel from './components/expansion-panel';
 
+const styles = {
+  'app': {
+      textAlign: 'center',
+      
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header/>
+      <div style={styles.app}>
+        <header>
+          <Header/>
+        </header>
         <SimpleExpansionPanel/>
         <footer>
           <Footer/>
