be425604884c69cec685e7025214e37474252c62
Adam Hasselberg <adamhasselberg@gmail.com>
2019-05-02 13:05:02 +0200

    Trying to fix material ui css issue

--- src/components/myButton-happo.jsx
+++ src/components/myButton-happo.jsx
@@ -3,6 +3,7 @@ import Button from "./myButton";
 import Header from "./header";
 import Footer from "./footer";
 import SimpleExpansionPanel from "./expansion-panel";
+import { withStyles } from '@material-ui/core/styles';
 
 
 export const primary = () => (
