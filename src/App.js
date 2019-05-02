d58896da104ab4a37781380a6bd7ef15723bb043
junglebooboo <aigarst@stud.ntnu.no>
2019-05-02 12:48:26 +0200

    removed some outside styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SimpleExpansionPanel from './components/expansion-panel';
 
 const styles = {
   'app': {
-      textAlign: 'center',
+    textAlign: 'center',
       
   }
 }
@@ -14,15 +14,15 @@ const styles = {
 class App extends Component {
   render() {
     return (
-      <div style={styles.app}>
-        <header>
-          <Header/>
-        </header>
-        <SimpleExpansionPanel/>
-        <footer>
-          <Footer/>
-        </footer>
-      </div>
+        <div style={styles.app}>
+          <header>
+            <Header/>
+          </header>
+          <SimpleExpansionPanel/>
+          <footer>
+            <Footer/>
+          </footer>
+        </div>
     );
   }
 }
--- src/components/myButton-happo.jsx
+++ src/components/myButton-happo.jsx
@@ -3,12 +3,7 @@ import Button from "./myButton";
 import Header from "./header";
 import Footer from "./footer";
 import SimpleExpansionPanel from "./expansion-panel";
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import Typography from '@material-ui/core/Typography';
+
 
 export const primary = () => (
     <Button type="primary" label="Primary"/>
