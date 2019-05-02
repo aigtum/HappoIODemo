4096aaab6ad52fd624c07e7c1b2009cd978e31ee
junglebooboo <aigarst@stud.ntnu.no>
2019-05-02 12:50:37 +0200

    removed css loader

--- .happo.js
+++ .happo.js
@@ -19,14 +19,5 @@ module.exports = {
   publicFolders: [
     path.resolve(__dirname, 'src/images'),
   ],
-
-  customizeWebpackConfig: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [{ loader: cssLoader }],
-    });
-   // it's important that we return the modified config
-    return config;
-  },
   
 };
