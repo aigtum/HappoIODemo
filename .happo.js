87a3bcdd48e5503f578acf77a705ad94769c0a56
junglebooboo <aigarst@stud.ntnu.no>
2019-05-02 13:12:09 +0200

    added css loader

--- .happo.js
+++ .happo.js
@@ -19,5 +19,13 @@ module.exports = {
   publicFolders: [
     path.resolve(__dirname, 'src/images'),
   ],
+  customizeWebpackConfig: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [{ loader: css-loader }],
+    });
+   // it's important that we return the modified config
+    return config;
+  },
   
 };
