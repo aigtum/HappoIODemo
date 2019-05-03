32336c71055614b9ba28defb1abd76d8f4d26a58
Adam Hasselberg <adamhasselberg@gmail.com>
2019-05-03 15:13:17 +0200

    Trying to deploy from different image

--- deployment.sh
+++ deployment.sh
@@ -9,7 +9,7 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME
 gcloud config set compute/zone $CLOUDSDK_COMPUTE_ZONE
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 # SKIP REBUILDING DOCKER IMAGE IN EVERY BUILD: 
-# docker build -t gcr.io/${PROJECT_ID}/${REG_ID}:$CIRCLE_SHA1 .
-# gcloud docker -- push gcr.io/${PROJECT_ID}/${REG_ID}:$CIRCLE_SHA1
+docker build -t gcr.io/${PROJECT_ID}/${REG_ID}:$CIRCLE_SHA1 .
+gcloud docker -- push gcr.io/${PROJECT_ID}/${REG_ID}:$CIRCLE_SHA1
 kubectl set image deployment/${DEPLOYMENT_NAME} ${CONTAINER_NAME}=eu.gcr.io/${PROJECT_ID}/${REG_ID}:${IMG_TAG}
 echo " Successfully deployed to ${DEPLOYMENT_ENVIRONMENT}"
