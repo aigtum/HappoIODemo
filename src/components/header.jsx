6b44bd1cb78e008281c56776382ca27ed6f19f91
Adam Hasselberg <adamhasselberg@gmail.com>
2019-05-03 14:38:46 +0200

    Logo loading

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Logo from '../images/logo.png';
 
 const styles = {
   'header': {
@@ -24,7 +25,7 @@ const Header = (props) => {
   return (
     <header style={styles.header}>
       <h1 style={styles.h1}>
-        <img style={styles.img} height={50} mode='fit' src="https://camo.githubusercontent.com/192f268f4e37ae06a0648e3e99a4923be7f45124/68747470733a2f2f686170706f2e696f2f7374617469632f686170706f2d686970706f2e706e67" alt="happo-logo"/>
+        <img style={styles.img} height={50} mode='fit' src={Logo} alt="happo-logo"/>
         Visual Regression Testing
       </h1>
       <h4 style={styles.h1}>With CircleCI and Happo.io</h4>
