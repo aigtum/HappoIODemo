b4966dab7b17d3d399939015de10f16398fdfad5
Adam Hasselberg <adamhasselberg@gmail.com>
2019-05-05 11:36:09 +0200

    Adding more CSS to the page

--- src/components/pipeline.jsx
+++ src/components/pipeline.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 const styles = {
   fontFamily: 'Noto Serif',
   paddingTop: '20px',
+  backgroundColor: 'white',
+  webkitBoxShadow: '3px 4px 20px -13px rgba(0,0,0,0.75)',
+  mozBoxShadow: '3px 4px 20px -13px rgba(0,0,0,0.75)',
+  boxShadow: '3px 4px 20px -13px rgba(0,0,0,0.75)',
+  borderBottom: '4px double black'
 }
 
 const Pipeline = (props) => {
